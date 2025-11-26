e4cc202d5f55fad65118a4de558d4f89e10a0aa5
no-kris <kris.tresk@gmail.com>
2025-11-26 15:01:52 -0500

    Refactor: remove unused app files and clean up navigation links in templates

--- suntransflask/app.py
+++ suntransflask/app.py
@@ -1,244 +1,221 @@
-from flask import Flask, jsonify, request, url_for, redirect, session, render_template
-import json
+from flask import Flask, request, url_for, session, render_template
+import json, os
 from html import escape
-import os.path
 from datetime import datetime
-import sys
-import time
-
-
 
 app = Flask(__name__, static_folder="../public_html/static", template_folder="templates")
 application = app
 
-# Config stuff goes here at the top before the routes.
-# The Secret Key is necessary for starting a session.
-app.config['DEBUG'] = True
-app.config['SECRET_KEY'] = 'Thisisasecret!'
+app.config.update(
+    DEBUG=True,
+    SECRET_KEY='Thisisasecret!',
+    TEMPLATES_AUTO_RELOAD=True
+)
 
-TEMPLATES_AUTO_RELOAD = True
-
-with open('./json/gl_active_list.json') as file:
-    menu_data = json.load(file)
-    
-with open('./json/SUN_BIBLE_page_list.json') as file:
-    menu_data1 = json.load(file)    
-
-# This is the "index.html" route.
-@app.route('/')
-def index():
-    #with open('./json/gl_active_list.json') as file:
-       #menu_data = json.load(file)
-    session.pop('name', None)
-    return render_template('index.html', title='SUN Translation Resources', navindex=True, json_data = menu_data, json_data1 = menu_data1 )
-    
-# This is the "where-page.html" route.
-@app.route('/where')
-def where():
-    #with open('./json/gl_active_list.json') as file:
-      #menu_data = json.load(file)
-    session.pop('name', None)
-    return render_template('where-org.html', title='projects', json_data = menu_data, json_data1 = menu_data1 )
-    
-# This is the "contact-teach.html" route.
-@app.route('/contact')
-def contact():
-    session.pop('name', None)
-    return render_template('contact-teach.html', title='Contact Us')
-    
-# This is the "contact-translator.html" route.
-@app.route('/contactus')
-def contactus():
-    session.pop('name', None)
-    return render_template('contact-translator.html', title='Contact Us')
-
-# This is the "terms.html" route.
-@app.route('/terms')
-def terms():
-    #with open('./json/gl_active_list.json') as file:
-      #menu_data = json.load(file)
-    session.pop('name', None)
-    return render_template('terms-conditions.html', title='Terms and Conditions', json_data = menu_data, json_data1 = menu_data1 )
+with open('./json/gl_active_list.json') as f:
+    nav_menu = json.load(f)
+with open('./json/SUN_BIBLE_page_list.json') as f:
+    sun_menu = json.load(f)
 
 
-# This is the "privacy.html" route.
-@app.route('/privacy')
-def privacy():
-    #with open('./json/gl_active_list.json') as file:
-      #menu_data = json.load(file)
-    session.pop('name', None)
-    return render_template('privacy-policy.html', title='Privacy Policy', json_data = menu_data, json_data1 = menu_data1 )
+def read_json(path):
+    """
+    Read the passed in JSON file.
 
-
-# This is the "status.html" route.
-@app.route('/status')
-def status():
-    session.pop('name', None)
-    with open('./json/lang_status.json','r') as file:
-      table_data = file.read()
-    html_table = json_to_html_table(table_data)
-    session.pop('name', None)
-    return render_template('status.html', title='Translation Status', html_table=html_table, json_data = menu_data, json_data1 = menu_data1 )
-
-# create HTML table.
-def json_to_html_table(table_data):
-    data = json.loads(table_data)
-
-    # Create HTML table header
-    table_html = "<table border='5'><tr>"
-    for key in data[0].keys():
-        table_html += f"<th>{escape(key)}</th>"
-    table_html += "</tr>"
-
-    # Create HTML table rows
-    for item in data:
-        table_html += "<tr>"
-        for value in item.values():
-            table_html += f"<td>{escape(str(value))}</td>"
-        table_html += "</tr>"
-
-    # Close HTML table
-    table_html += "</table>"
-
-    return table_html
-    
-@app.route('/process', methods=['POST'])
-def process_data():
-    session.pop('name', None)
-    selected_option = request.form.get('selected_option')
-    return f'The selected option is: {selected_option}'    
-    
-# This is the "partners.html" route.
-@app.route('/partners')
-def partners():
-    session.pop('name', None)
-    with open('./json/web_partner_list.json','r') as file:
-     table_data = file.read()
-    html_table = json_to_html_table(table_data)
-    session.pop('name', None)
-    return render_template('partners.html', title='partners',html_table=html_table, json_data = menu_data, json_data1 = menu_data1 )
-
-# This is the "resource-page.html" route.
-@app.route('/resource/<code>', methods=["GET", "POST"])
-def resource_page(code):
-    session.pop('name', None)
-    json_data = read_json_file(code)
-    html_content = generate_html_from_json(json_data)
-    session.pop('name', None)
-    return render_template('resource.html', title='Resource Files', html_content=html_content, code=code, json_data = menu_data, json_data1 = menu_data1 )
-    
-def read_json_file(received_code):
-    file_name = f"./json/{received_code}-page.json"
-
+    Returns:
+        Object: The contents from the passed in JSON file.
+    """
     try:
-        with open(file_name, 'r') as file:
-            data = json.load(file)
-            return data
+        with open(path, 'r') as f:
+            return json.load(f)
     except FileNotFoundError:
         return None
-        
-# This is the "sun-page.html" route.
-@app.route('/sun/<code>', methods=["GET", "POST"])
-def sun_page(code):
-    session.pop('name', None)
-    json_data1 = read_json_file1(code)
-    html_content = generate_html_from_json1(json_data1)
-    session.pop('name', None)
-    return render_template('sun.html', title='Sun Files', html_content=html_content,code=code, json_data = menu_data, json_data1 = menu_data1 )
+
+
+def json_to_html_table(table_data):
+    """
+    Converts a JSON object, represented as a list of key:value pairs, as an HTML table.
+
+    Returns:
+        HTML markup table
+    """
+    data = json.loads(table_data)
+    header = ''.join(f"<th>{escape(k)}</th>" for k in data[0].keys())
+    rows = ''.join(
+        "<tr>" + ''.join(f"<td>{escape(str(v))}</td>" for v in item.values()) + "</tr>"
+        for item in data
+    )
+    return f"<table border='5'><tr>{header}</tr>{rows}</table>"
+
+
+def generate_html_list(items, subfolder):
+    """
+    Generates an HTML unordered list of downlaodable files.
+
+    Args:
+        items (list): List of downloadable files.
+        subfolder (str): Subfolder within the '/home/suntrans/public_html/static/' directory. 
+
+    Returns:
+        HTML markup for the list of download links.
+    """
+    html = ""
+    for entry in items:
+        label, filename = entry['File Label'], entry['Download File Name']
+        target_path = f"/home/suntrans/public_html/static/{subfolder}/{filename}"
+        link = url_for('static', filename=f"{subfolder}/{filename}")
+        if os.path.exists(target_path):
+            date = datetime.fromtimestamp(os.path.getmtime(target_path)).strftime('%Y-%m-%d')
+        else:
+            date = "Coming soon"
+        html += f"<li><i class='fas fa-square'></i><a href='{link}' target='_blank'>{label}</a> - {date}</li>\n"
+    return html
+
+
+def render_page(template, title, **kwargs):
+    """
+    Helper function for flask's render_template function. 
+    Injects the shared navigation menu and the sun navigation menu into render_template.
     
-def read_json_file1(received_code):
-    file_name = f"./json/{received_code}-page.json"
+    Args:
+        template (str): Template file name (e.g. 'Index.html')
+        title (str): Page title
 
-    try:
-        with open(file_name, 'r') as file:
-            data = json.load(file)
-            return data
-    except FileNotFoundError:
-        return None    
-        
-# This is the "ENG-page.html" route.
-@app.route('/ENG')
-def ENG():
+    Returns:
+        Response: Rendered HTML response for the specific template.
+    """
+    return render_template(template, title=title, json_data=nav_menu, json_data1=sun_menu, **kwargs)
+
+
+@app.before_request
+def clear_name_session():
+    """
+    Clear the section key 'name' before every page request.
+    """
     session.pop('name', None)
-    return render_template('ENG-page.html', title='ENG Resource Files')
-
-# This is the "Testy-page.html" route.
-@app.route('/TESTY')
-def TESTY():
-    session.pop('name', None)
-    return render_template('testy-page.html', title='TESTY Resource Files')
 
 
-# This is the "PORB-page.html" route.
-@app.route('/PORB')
-def PORB():
-    session.pop('name', None)
-    return render_template('PORB-page.html', title='PORB Resource Files')
+@app.route('/')
+@app.route('/home')
+def index():
+    """
+    URL handler for the website 'index.html' home page.
+    """
+    return render_page('index.html', 'SUN Translation Resources', navindex=True)
 
+@app.route('/where')
+def where():
+    """
+    URL handler for the website 'where-org.html' page.
+    """
+    return render_page('where-org.html', 'Projects')
 
-# This is the "Bible-page.html" route.
+@app.route('/contact')
+def contact():
+    """
+    URL handler for the website 'contact-teach.html' page.
+    """
+    return render_template('contact-teach.html', title='Contact Us')
+
+@app.route('/contactus')
+def contactus():
+    """
+    URL handler for the website 'contact-translator.html' page.
+    """
+    return render_template('contact-translator.html', title='Contact Us')
+
+@app.route('/terms')
+def terms():
+    """
+    URL handler for the website 'terms-conditions.html' page.
+    """
+    return render_page('terms-conditions.html', 'Terms and Conditions')
+
+@app.route('/privacy')
+def privacy():
+    """
+    URL handler for the website 'privacy-policy.html' page.
+    """
+    return render_page('privacy-policy.html', 'Privacy Policy')
+
+@app.route('/status')
+def status():
+    """
+    URL handler for the website 'status.html' page.
+    The contents of the status table will be read from 'lang_status.json' file
+    found in the json folder.
+    """
+    with open('./json/lang_status.json') as f:
+        html_table = json_to_html_table(f.read())
+    return render_page('status.html', 'Translation Status', html_table=html_table)
+
+@app.route('/partners')
+def partners():
+    """
+    URL handler for the website 'partners.html' page.
+    The contents of the status table will be read from 'web_partner_list.json' file
+    found in the json folder.
+    """
+    with open('./json/web_partner_list.json') as f:
+        html_table = json_to_html_table(f.read())
+    return render_page('partners.html', 'Partners', html_table=html_table)
+
+@app.route('/resource/<code>')
+def resource_page(code):
+    """
+    Handles pages for downloadable resource files.
+
+    Loads a JSON file named '<code>-page.json' from the 'json' folder, then
+    builds a list of resource links with file names in the specific language.
+
+    Args:
+        code (str): The resource code specifying the langauge (e.g. "ENG", "PORB").
+
+    Returns:
+        Rendered HTML page showing available learning resource files.
+    """
+    data = read_json(f'./json/{code}-page.json')
+    html_content = generate_html_list(data, 'resource') if data else 'No data available.'
+    return render_page('resource.html', 'Resource Files', html_content=html_content, code=code)
+
+@app.route('/sun/<code>')
+def sun_page(code):
+    """
+    Handles pages for downloadable Bible books.
+
+    Loads a JSON file named '<code>-page.json' from the 'json' folder, then
+    builds a list of Bible files in the specific language.
+
+    Args:
+        code (str): The resource code specifying the langauge (e.g. "ENG", "PORB").
+
+    Returns:
+        Rendered HTML page showing available Bible books for downlaod.
+    """
+    data = read_json(f'./json/{code}-page.json')
+    html_content = generate_html_list(data, 'bible') if data else 'No data available.'
+    return render_page('sun.html', 'Sun Files', html_content=html_content, code=code)
+
 @app.route('/Bible')
 def Bible():
-    with open('./json/gl_active_list.json') as file:
-     json_data = json.load(file)
-    session.pop('name', None)
+    """
+    URL handler for the website 'Bible-page.html' page.
+    The contents of the status table will be read from 'gl_active_list.json' file
+    found in the json folder.
+    """
+    with open('./json/gl_active_list.json') as f:
+        json_data = json.load(f)
     return render_template('Bible-page.html', title='SUN Translation Resources', json_data=json_data)
-   # return render_template('Bible-page.html', title="SUN Bible Files")
-    
+
 @app.errorhandler(404)
-def page_not_found(e):
+def not_found(e):
+    """
+    Error handler for missing pages. 
+
+    Returns:
+        Response: Custom '404.html' page and HTTP 404 status.
+    """
     return render_template('404.html'), 404
-
-def generate_html_from_json(html_data):
-    # Customize this function based on your JSON structure and HTML template
-    # For simplicity, it assumes a basic structure with links to download PDF files
-    html_content = ""
-    for entry in html_data:
-        file_name = entry['File Label']
-        download_file = entry['Download File Name']
-        target_url = url_for('static', filename='resource/' + download_file)
-        download_link = target_url
-        target_directory = '/home/suntrans/public_html/static/resource/'
-        target_path = os.path.join(target_directory, download_file)
-        if os.path.exists(target_path):
-            last_modified_timestamp = os.path.getmtime(target_path)
-            last_modified_date = datetime.fromtimestamp(last_modified_timestamp).date()
-            last_modified_date_str = last_modified_date.strftime('%Y-%m-%d')
-        else:
-            last_modified_date_str = "Coming soon"
-        html_content += f"        <li><i class='fas fa-square'></i><a href='{download_link}' target='_blank'>{file_name}</a> - {last_modified_date_str}</li>\n"
-    return html_content
-    
-def generate_html_from_json1(html_data):
-    # Customize this function based on your JSON structure and HTML template
-    # For simplicity, it assumes a basic structure with links to download PDF files
-    html_content = ""
-    for entry in html_data:
-        file_name = entry['File Label']
-        download_file = entry['Download File Name']
-        target_url = url_for('static', filename='bible/' + download_file)
-        download_link = target_url
-        target_directory = '/home/suntrans/public_html/static/bible/'
-        target_path = os.path.join(target_directory, download_file)
-        if os.path.exists(target_path):
-            last_modified_timestamp = os.path.getmtime(target_path)
-            last_modified_date = datetime.fromtimestamp(last_modified_timestamp).date()
-            last_modified_date_str = last_modified_date.strftime('%Y-%m-%d')
-        else:
-            last_modified_date_str = "Coming soon"
-        html_content += f"        <li><i class='fas fa-square'></i><a href='{download_link}' target='_blank'>{file_name}</a> - {last_modified_date_str}</li>\n"
-    return html_content    
-
-# GET is the default method if nothing is specified.
-# Notice that a session name variable gets set in this route.
-# (leaving this old example code here in case it helps... it shows
-# methods, defaults, and passing arguments for render_template)
-
-# @app.route('/home', methods=['POST', 'GET'], defaults={'name' : 'Default'})
-# @app.route('/home/<string:name>', methods=['POST', 'GET'])
-# def home(name):
-# session['name'] = name
-# return render_template('home.html', name=name, display=False) # , mylist=['one', 'two', 'three', 'four'], listofdictionaries=[{'name' : 'Zach'}, {'name' : 'Zoe'}])
 
 if __name__ == '__main__':
     app.run()
--- suntransflask/appcopy.py
+++ suntransflask/appcopy.py
@@ -1,14 +1,10 @@
-from flask import Flask, jsonify, request, url_for, redirect, session, render_template
+from flask import Flask, request, url_for, session, render_template
 import json
 from html import escape
 import os.path
 from datetime import datetime
-import sys
-import time
 
-
-
-app = Flask(__name__)
+app = Flask(__name__, static_folder="../public_html/static", template_folder="templates")
 application = app
 
 # Config stuff goes here at the top before the routes.
@@ -20,6 +16,9 @@ TEMPLATES_AUTO_RELOAD = True
 
 with open('./json/gl_active_list.json') as file:
     menu_data = json.load(file)
+    
+with open('./json/SUN_BIBLE_page_list.json') as file:
+    menu_data1 = json.load(file)    
 
 # This is the "index.html" route.
 @app.route('/')
@@ -27,7 +26,7 @@ def index():
     #with open('./json/gl_active_list.json') as file:
        #menu_data = json.load(file)
     session.pop('name', None)
-    return render_template('index.html', title='SUN Translation Resources', navindex=True, json_data = menu_data )
+    return render_template('index.html', title='SUN Translation Resources', navindex=True, json_data = menu_data, json_data1 = menu_data1 )
     
 # This is the "where-page.html" route.
 @app.route('/where')
@@ -35,15 +34,15 @@ def where():
     #with open('./json/gl_active_list.json') as file:
       #menu_data = json.load(file)
     session.pop('name', None)
-    return render_template('where-org.html', title='projects', json_data = menu_data )
+    return render_template('where-org.html', title='projects', json_data = menu_data, json_data1 = menu_data1 )
     
-    # This is the "contact-teach.html" route.
+# This is the "contact-teach.html" route.
 @app.route('/contact')
 def contact():
     session.pop('name', None)
     return render_template('contact-teach.html', title='Contact Us')
     
-     # This is the "contact-translator.html" route.
+# This is the "contact-translator.html" route.
 @app.route('/contactus')
 def contactus():
     session.pop('name', None)
@@ -55,7 +54,7 @@ def terms():
     #with open('./json/gl_active_list.json') as file:
       #menu_data = json.load(file)
     session.pop('name', None)
-    return render_template('terms-conditions.html', title='Terms and Conditions', json_data = menu_data )
+    return render_template('terms-conditions.html', title='Terms and Conditions', json_data = menu_data, json_data1 = menu_data1 )
 
 
 # This is the "privacy.html" route.
@@ -64,19 +63,20 @@ def privacy():
     #with open('./json/gl_active_list.json') as file:
       #menu_data = json.load(file)
     session.pop('name', None)
-    return render_template('privacy-policy.html', title='Privacy Policy', json_data = menu_data )
+    return render_template('privacy-policy.html', title='Privacy Policy', json_data = menu_data, json_data1 = menu_data1 )
 
 
 # This is the "status.html" route.
 @app.route('/status')
 def status():
+    session.pop('name', None)
     with open('./json/lang_status.json','r') as file:
       table_data = file.read()
     html_table = json_to_html_table(table_data)
     session.pop('name', None)
-    return render_template('status.html', title='Translation Status',html_table=html_table, json_data = menu_data )
+    return render_template('status.html', title='Translation Status', html_table=html_table, json_data = menu_data, json_data1 = menu_data1 )
 
-    # create HTML table.
+# create HTML table.
 def json_to_html_table(table_data):
     data = json.loads(table_data)
 
@@ -100,25 +100,28 @@ def json_to_html_table(table_data):
     
 @app.route('/process', methods=['POST'])
 def process_data():
+    session.pop('name', None)
     selected_option = request.form.get('selected_option')
     return f'The selected option is: {selected_option}'    
     
-  # This is the "partners.html" route.
+# This is the "partners.html" route.
 @app.route('/partners')
 def partners():
+    session.pop('name', None)
     with open('./json/web_partner_list.json','r') as file:
      table_data = file.read()
     html_table = json_to_html_table(table_data)
     session.pop('name', None)
-    return render_template('partners.html', title='partners',html_table=html_table, json_data = menu_data )
+    return render_template('partners.html', title='partners',html_table=html_table, json_data = menu_data, json_data1 = menu_data1 )
 
 # This is the "resource-page.html" route.
 @app.route('/resource/<code>', methods=["GET", "POST"])
 def resource_page(code):
+    session.pop('name', None)
     json_data = read_json_file(code)
     html_content = generate_html_from_json(json_data)
     session.pop('name', None)
-    return render_template('resource.html', title='Resource Files', navindex=True, html_content=html_content,code=code, json_data = menu_data )
+    return render_template('resource.html', title='Resource Files', html_content=html_content, code=code, json_data = menu_data, json_data1 = menu_data1 )
     
 def read_json_file(received_code):
     file_name = f"./json/{received_code}-page.json"
@@ -129,6 +132,25 @@ def read_json_file(received_code):
             return data
     except FileNotFoundError:
         return None
+        
+# This is the "sun-page.html" route.
+@app.route('/sun/<code>', methods=["GET", "POST"])
+def sun_page(code):
+    session.pop('name', None)
+    json_data1 = read_json_file1(code)
+    html_content = generate_html_from_json1(json_data1)
+    session.pop('name', None)
+    return render_template('sun.html', title='Sun Files', html_content=html_content,code=code, json_data = menu_data, json_data1 = menu_data1 )
+    
+def read_json_file1(received_code):
+    file_name = f"./json/{received_code}-page.json"
+
+    try:
+        with open(file_name, 'r') as file:
+            data = json.load(file)
+            return data
+    except FileNotFoundError:
+        return None    
         
 # This is the "ENG-page.html" route.
 @app.route('/ENG')
@@ -182,6 +204,26 @@ def generate_html_from_json(html_data):
             last_modified_date_str = "Coming soon"
         html_content += f"        <li><i class='fas fa-square'></i><a href='{download_link}' target='_blank'>{file_name}</a> - {last_modified_date_str}</li>\n"
     return html_content
+    
+def generate_html_from_json1(html_data):
+    # Customize this function based on your JSON structure and HTML template
+    # For simplicity, it assumes a basic structure with links to download PDF files
+    html_content = ""
+    for entry in html_data:
+        file_name = entry['File Label']
+        download_file = entry['Download File Name']
+        target_url = url_for('static', filename='bible/' + download_file)
+        download_link = target_url
+        target_directory = '/home/suntrans/public_html/static/bible/'
+        target_path = os.path.join(target_directory, download_file)
+        if os.path.exists(target_path):
+            last_modified_timestamp = os.path.getmtime(target_path)
+            last_modified_date = datetime.fromtimestamp(last_modified_timestamp).date()
+            last_modified_date_str = last_modified_date.strftime('%Y-%m-%d')
+        else:
+            last_modified_date_str = "Coming soon"
+        html_content += f"        <li><i class='fas fa-square'></i><a href='{download_link}' target='_blank'>{file_name}</a> - {last_modified_date_str}</li>\n"
+    return html_content    
 
 # GET is the default method if nothing is specified.
 # Notice that a session name variable gets set in this route.
@@ -192,7 +234,7 @@ def generate_html_from_json(html_data):
 # @app.route('/home/<string:name>', methods=['POST', 'GET'])
 # def home(name):
 # session['name'] = name
-# return render_template('home.html', name=name, display=False) # , mylist=['one', 'two', 'three', 'four'], listofdictionaries=[{'name' : 'Zach'}, {'name' : 'Zoe'}])
+# return render_template('home.html', name=name, display=False) # , mylist=['one', 'two', 'three', 'four'], listofdictionaries=[{'name' : 'Zach'}, {'name': 'Zoe'}])
 
-if __name__ == '__main__':
-    app.run()
+# if __name__ == '__main__':
+#     app.run()
