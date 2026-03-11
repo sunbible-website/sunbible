c28ed7f47b105b091baf63c527d60118f1cecb66
no-kris <kris.tresk@gmail.com>
2026-03-11 16:33:20 -0400

    Update passenger_wsgi.py

--- suntransflask/passenger_wsgi.py
+++ suntransflask/passenger_wsgi.py
@@ -1,10 +1,12 @@
-import sys
 import os
+import sys
 
 # Add the current directory (project root) to Python path
 project_home = os.path.dirname(os.path.abspath(__file__))
 if project_home not in sys.path:
     sys.path.insert(0, project_home)
 
+
+os.chdir(project_home)
 
 from app import app as application
